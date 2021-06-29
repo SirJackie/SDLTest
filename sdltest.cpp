0a69ce2bc245e0891ea52be7bbba839155dad0d3
lyj00912 <3122742760@qq.com>
2021-06-29 15:34:36 +0800

    created .gitignore and successfully shown an BMP image

--- sdltest.cpp
+++ sdltest.cpp
@@ -1,61 +1,120 @@
-//使用 SDL 和 标准 IO
-#include <SDL.h>
 #include <stdio.h>
+#include <SDL.h>
 
-//定义屏幕尺寸常量
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+#define SCREEN_WIDTH  640
+#define SCREEN_HEIGHT 480
 
-int main( int argc, char* args[] )
+//Starts up SDL and creates window
+//函数功能：初始化SDL并创建窗口
+bool init();
+
+//Loads media
+//函数功能：加载多媒体文件
+bool loadMedia();
+
+//Frees media and shuts down SDL
+//函数功能：释放多媒体文件并关闭SDL
+void close();
+
+//初始化我们将要渲染的窗口
+SDL_Window* gWindow = NULL;
+
+//The surface contained by the window
+//窗口包含的surface
+SDL_Surface* gScreenSurface = NULL;
+
+//我们将会载入并在屏幕上展示的图片
+SDL_Surface* gHelloWorld = NULL;
+
+bool init()
 {
-    //将要渲染的窗口
-    SDL_Window* window = NULL;
+    //初始化标记变量
+    bool success = true;
 
-    //窗口含有的surface
-    SDL_Surface* screenSurface = NULL;
-
-    //初始化SDL
+    //初始化 SDL
     if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
     {
         printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+        success = false;
     }
-
-	else
+    else
     {
         //创建 window
-        window = SDL_CreateWindow
-        (
-            "SDL Tutorial",           // Title
-            SDL_WINDOWPOS_UNDEFINED,  // Left Margin
-            SDL_WINDOWPOS_UNDEFINED,  // Top Margin
-            SCREEN_WIDTH,             // Window Width
-            SCREEN_HEIGHT,            // Window Height
-            SDL_WINDOW_SHOWN
-        );
-        if( window == NULL )
+        gWindow = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( gWindow == NULL )
         {
             printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+            success = false;
         }
-		else
+        else
         {
             //获取 window surface
-            screenSurface = SDL_GetWindowSurface( window );
-
-            //用白色填充surface
-            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
-
-            //更新surface
-            SDL_UpdateWindowSurface( window );
-            
-            //延迟两秒
-            SDL_Delay( 2000 );
+            gScreenSurface = SDL_GetWindowSurface( gWindow );
         }
     }
-    //销毁 window
-    SDL_DestroyWindow( window );
+
+    return success;
+}
+
+#define BMP_ADDR "hw.bmp"
+
+bool loadMedia()
+{
+    //用来标记载入成功的变量
+    bool success = true;
+
+    //载入 splash image
+    gHelloWorld = SDL_LoadBMP(BMP_ADDR);
+    if( gHelloWorld == NULL )
+    {
+        printf( "Unable to load image %s! SDL Error: %s\n", BMP_ADDR, SDL_GetError() );
+        success = false;
+    }
+
+    return success;
+}
+
+void close()
+{
+    //释放 surface
+    SDL_FreeSurface( gHelloWorld );
+    gHelloWorld = NULL;
+
+    //销毁窗口
+    SDL_DestroyWindow( gWindow );
+    gWindow = NULL;
 
     //退出 SDL subsystems
     SDL_Quit();
+}
+
+int main( int argc, char* args[] )
+{
+    //初始化 SDL 并创建窗口
+    if( !init() )
+    {
+        printf( "Failed to initialize!\n" );
+    }
+    else
+    {
+        //加载多媒体文件
+        if( !loadMedia() )
+        {
+            printf( "Failed to load media!\n" );
+        }
+        else
+        {
+            //应用这个图像
+            SDL_BlitSurface( gHelloWorld, NULL, gScreenSurface, NULL );
+            //更新surface
+            SDL_UpdateWindowSurface( gWindow );
+            //保持窗口两秒
+            SDL_Delay( 2000 );
+        }
+    }
+
+    //释放资源并关闭SDL
+    close();
 
     return 0;
 }
